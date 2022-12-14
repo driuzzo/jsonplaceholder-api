5f71e1290c2b2635a9217710ba66f153e1144ae2
Adriano Driuzzo <adriano.driuzzo@mzgroup.com>
2022-12-13 21:23:32 -0300

    updating tests

--- cypress/e2e/jsonplaceholder.cy.js
+++ cypress/e2e/jsonplaceholder.cy.js
@@ -1,5 +1,9 @@
 /// <reference types="cypress" />
 
+const { faker } = require('@faker-js/faker')
+let postTitle
+const postId = 1
+
 describe("happy path api tests", () => {
 
     it('gets all resources', () => {
@@ -13,30 +17,64 @@ describe("happy path api tests", () => {
 
     it('gets a todo', () => {
 
-        cy.api('GET', 'https://jsonplaceholder.typicode.com/todos/1')
+        cy.api('GET', `https://jsonplaceholder.typicode.com/posts/${postId}`)
             .then((response) => {
+                postTitle = response.body.title
                 expect(response.status).to.eq(200)
                 expect(response.body.userId).to.eq(1)
+                cy.wrap(postTitle)
             })
     })
 
     it('creates a resource', () => {
+
+        const title = faker.random.words(2)
+        const body = faker.random.words(4)
         
         cy.api('POST', 'https://jsonplaceholder.typicode.com/posts', {
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
             },
             body: {
-                title: 'QA',
-                body: 'Driuzzo',
+                title: title,
+                body: body,
                 userId: 1
             }
         })
         .then((response) => {
             console.log(response)
             expect(response.status).to.eq(201)
-            expect(response.body.body.title).to.eq('QA')
-            expect(response.body.body.body).to.eq('Driuzzo')
+            expect(response.body.body.title).to.eq(title)
+            expect(response.body.body.body).to.eq(body)
         })
     })
+
+    it('updates a resource', () => {
+
+        const title = faker.random.words(2)
+        const body = faker.random.words(4)
+
+        cy.api('PUT', `https://jsonplaceholder.typicode.com/posts/${postId}`, {
+            body: {
+                title: title,
+                body: body,
+                userId: 1
+            }
+        })
+        .then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body.body.title).to.eq(title)
+            expect(response.body.body.body).to.eq(body)
+            expect(response.body.body.title).not.to.eq(postTitle)
+        })
+    })
+
+    it('deletes a resource', () => {
+
+        cy.api('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then((response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body).to.be.empty
+            })
+        })
 })
